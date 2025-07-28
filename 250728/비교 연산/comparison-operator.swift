f33674d0c6440d7fe8f926d46f9b3ff329ebaa77
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-28 11:22:27 +0900

    Title: 비교 연산, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250728/비교 연산/comparison-operator.swift
@@ -0,0 +1,42 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let a = input[0]
+let b = input[1]
+
+if (1 <= a && a <= 100) && (1 <= b && b <= 100) {
+    if a >= b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+    if a > b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+    if a <= b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+    if a < b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+    if a == b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+    if a != b {
+        print(1)
+    }
+    else {
+        print(0)
+    }
+}
