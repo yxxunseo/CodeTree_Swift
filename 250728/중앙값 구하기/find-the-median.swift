c8eed57162e7745135729f9e4bbe67e54ef5a5f4
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-28 20:28:26 +0900

    Title: 중앙값 구하기, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250728/중앙값 구하기/find-the-median.swift
@@ -0,0 +1,11 @@
+let input = readLine()!.split(separator: " ").map { Int(String($0))! }
+let a = input[0]
+let b = input[1]
+let c = input[2]
+
+if (-100 <= a && a <= 100) && (-100 <= b && b <= 100) && (-100 <= c && c <= 100) {
+    var arr = [a, b, c]
+    arr.sort()
+
+    print(arr[1])
+}
