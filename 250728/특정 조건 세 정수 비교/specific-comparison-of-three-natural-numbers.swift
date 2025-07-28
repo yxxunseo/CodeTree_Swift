81542b275f8bae9bcc82f8da643c88e6773c440f
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-28 14:15:22 +0900

    Title: 특정 조건 세 정수 비교, Time: 10ms, Memory: 1MB, Status: Wrong Answer - Codetree

--- 250728/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.swift
+++ 250728/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.swift
@@ -7,7 +7,7 @@ var result1 = 0
 var result2 = 0
 
 if (1 <= a && a <= 100) && (1 <= b && b <= 100) && (1 <= c && c <= 100) {
-    if a < b {
+    if a < b && a == b{
         if a < c {
             result1 = 1
         }
