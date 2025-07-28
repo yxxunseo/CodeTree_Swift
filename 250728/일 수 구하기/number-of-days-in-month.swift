fb2df99a72759101e326b0b0f328541c57d45d04
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-28 19:55:28 +0900

    Title: 일 수 구하기, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

--- 250728/일 수 구하기/number-of-days-in-month.swift
+++ 250728/일 수 구하기/number-of-days-in-month.swift
@@ -4,7 +4,10 @@ if (1 <= n && n <= 12) {
     if n == 2 {
         print("28")
     }
-    else {
+    else if n == 1 || n == 3 || n == 5 || n == 7 || n == 8 || n == 10 || n == 12{
         print("31")
+    }
+    else {
+        print("30")
     }
 }
