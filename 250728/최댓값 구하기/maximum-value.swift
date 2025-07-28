396dc295fdd5b21dea16a464ef5f0a6f5899ca6b
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-28 20:02:30 +0900

    Title: 최댓값 구하기, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

--- 250728/최댓값 구하기/maximum-value.swift
+++ 250728/최댓값 구하기/maximum-value.swift
@@ -6,7 +6,7 @@ let c = input[2]
 if (-100 <= a && a <= 100) && (-100 <= b && b <= 100) && (-100 <= c && c <= 100) {
     if a > b {
         if a > c {
-            print(c)
+            print(a)
         }
         else {
             print(c)
