e1e7afcef4bfa446b1740be87cf82745366612a8
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-29 12:41:47 +0900

    Title: 홀수만 출력, Time: 10ms, Memory: 1MB, Status: Passed - Codetree

--- 250729/홀수만 출력/output-only-odd.swift
+++ 250729/홀수만 출력/output-only-odd.swift
@@ -3,7 +3,7 @@ let a = input[0]
 let b = input[1]
 
 if (1 <= a && a <= 100) && (1 <= b && b <= 100) && (a % 2 == 1) && (a % 2 == 1) {
-    for i in stride(from: a, to: b + 1, by: 2) {
+    for i in stride(from: a, through: b, by: 2) {
         print(i, terminator: " ")
     } 
 }
