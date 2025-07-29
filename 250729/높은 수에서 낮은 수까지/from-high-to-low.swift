919a1aaae413a1c9e6cd1ed003e5f47cc1495757
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-07-29 20:48:05 +0900

    Title: 높은 수에서 낮은 수까지, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 250729/높은 수에서 낮은 수까지/from-high-to-low.swift
+++ 250729/높은 수에서 낮은 수까지/from-high-to-low.swift
@@ -1,19 +1,11 @@
 let input = readLine()!.split(separator: " ").map { Int(String($0))! }
 let a = input[0]
 let b = input[1]
+let maxVar = max(a, b)
+let minVar = min(a, b)
 
 if (1 <= a && a <= 50) && (1 <= b && b <= 50) {
-    if a > b {
-        for i in stride(from: a, through: b, by: -1) {
-            print(i, terminator: " ")
-        }
-    }
-    if a < b {
-        for i in stride(from: b, through: a, by: -1) {
-            print(i, terminator: " ")
-        }
-    }
-    if a == b {
-        print(a)
+    for i in stride(from: maxVar, through: minVar, by: -1) {
+        print(i, terminator: " ")
     }
 }
