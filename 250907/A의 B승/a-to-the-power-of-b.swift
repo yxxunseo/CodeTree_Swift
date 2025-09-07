67a33a044868c45dee68b5dd0a895231cee71df3
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-09-07 23:27:27 +0900

    Title: A의 B승, Time: 11ms, Memory: 1MB, Status: Passed - Codetree

--- 250907/A의 B승/a-to-the-power-of-b.swift
+++ 250907/A의 B승/a-to-the-power-of-b.swift
@@ -4,11 +4,12 @@ let b = Int(input[1])!
 var prod = 1
 
 if (1 <= a && a <= 10) && (0 <= b && b <= 8) {
-    if b != 0 {
+    if b == 0 {
+        print(1)
+    } else {
         for i in 1...b {
             prod *= a
         }
-        print(1)
+        print(prod)
     }
-    print(prod)
 }
