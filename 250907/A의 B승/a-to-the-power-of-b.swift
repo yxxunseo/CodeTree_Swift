7d7e7864fb05abc76211cd6a2681638066188cb1
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-09-07 23:21:36 +0900

    Title: A의 B승, Time: 19ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 250907/A의 B승/a-to-the-power-of-b.swift
@@ -0,0 +1,11 @@
+let input = readLine()!.split(separator: " ")
+let a = Int(input[0])!
+let b = Int(input[1])!
+var prod = 1
+
+if (1 <= a && a <= 10) && (0 <= b && b <= 8) {
+    for i in 1...b {
+        prod *= a
+    }
+    print(prod)
+}
