c4623be80756cab910b16b7879c474bac719dd30
Yunseo <161916112+yxxunseo@users.noreply.github.com>
2025-09-07 23:37:08 +0900

    Title: 친근하지 않은 수, Time: 11ms, Memory: 1MB, Status: Wrong Answer - Codetree

---
+++ 250907/친근하지 않은 수/unfriendly-number.swift
@@ -0,0 +1,14 @@
+let n = Int(readLine()!)!
+var cnt = 0
+
+if 1 <= n && n <= 1000 {
+    for i in 1...n {
+        if (i % 2 != 0 && i % 3 != 0) {
+            continue
+            if (i % 5 != 0) {
+                cnt += 1
+            }
+        }
+    }
+    print(cnt)
+}
